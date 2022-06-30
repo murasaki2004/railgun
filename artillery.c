ab11554aae3d43e2c0bc596bca7a8fb4b9ffd4bc
murasaki2004 <murasaki_jue@petalmail.com>
2022-06-30 15:59:11 +0800

    。

--- artillery.c
+++ artillery.c
@@ -13,6 +13,8 @@
 #define V_0 10
 //sqrt(2*x_def*b_def*i_def*l_def/m_def)
 
+
+// 四舍五入
 int rounded(double input){
     if (input < 0)
         return input;
@@ -20,29 +22,31 @@ int rounded(double input){
     return re;
 }
 
+// 射程上限
 double range_max(void){
-    //射程上限
     double v = V_0*V_0;
     v = v/r_g;
     return v;
 }
 
+
+// 平面角度
 double angle(double x, double y){
-    //仅限一、二象限
-    //角度位相对X轴正方向的角度
+    // 仅限一、二象限
+    // 角度位相对X轴正方向的角度
     if (y > 0){
         bool tmp = false;
-        //去符号
+        // 去符号
         if (x < 0){
             x = sqrt(pow(x, 2.0));
             tmp = true;
         }
-        //计算另一条边长
+        // 计算另一条边长
         double c = sqrt(x*x + y*y);
-        //用反正弦计算相对X轴的偏转弧度
+        // 用反正弦计算相对X轴的偏转弧度
         double angle = asin(y / c);
 
-        //把弧度转换成角度后输出，第一象限补上90°
+        // 把弧度转换成角度后输出，第一象限补上90°
         if (tmp)
             return (angle * (180 / M_PI)) + 90;
         else
@@ -53,19 +57,19 @@ double angle(double x, double y){
 }
 
 double placement(double x, double y, double c){
-    //验证数据合法
+    // 验证数据合法
     if (y < 0 || c < 0)
         return -1;
     else if (c+x+y == 0)
         return -1;
 
-    //如果传入xy则计算c并判断射程上限
+    // 如果传入xy则计算c并判断射程上限
     if (c == 0)
         c = sqrt(x*x + y*y);
     if (c > range_max())
         return -1;
 
-    //计算倾角
+    // 计算倾角
     double angle = asin((c * r_g) / pow(V_0, 2.0));
     angle = angle * (180 / M_PI) / 2;
     return angle;
